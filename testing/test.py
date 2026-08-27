68a33e602c3377ba405d9b070729103aa5ea0978
kevpot404 <potkev@proton.me>
2026-08-27 16:31:27 +0300

    implement test.py

---
+++ testing/test.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import joblib
+
+from training.preprocess import extract_features
+
+MODEL_PATH = "model.pkl"
+DATASET_PATH = "external_dataset.csv"
+
+model = joblib.load(MODEL_PATH)
+
+df = pd.read_csv(DATASET_PATH)
+
+X = pd.DataFrame(
+    df["URL"].apply(extract_features).tolist()
+)
+
+y = df["label"]
+
+predictions = model.predict(X)
+probabilities = model.predict_proba(X)[:, 1]
